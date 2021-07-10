1be49293fb1e04d2e243348faca933d62de09ab3
Luis Canada <80722367+luis-c465@users.noreply.github.com>
2021-07-10 15:21:14 +0000

    Complete exercise

--- lab2.py
+++ lab2.py
@@ -4,27 +4,28 @@
 once = "umbr"
 repeat = "ella"
 
-rihanna_says = ...
+# Store string concated string in rihanna_says and return result
+print( (rihanna_says := once + (repeat + " ") * 3) )
 
 # Hint: Use both the addition and multiplication signs to make your code concise
 
 # 2.
 # Find values for x such that each print statement is reached.
-
-x = ...
+correct_nums = [0, 1, 5, 6]
 y = 5
+for x in [0, 1, 5, -6]:
+    if -1 < x / y < 1:
+        if not x:
+            print("Found me! x is ", x)
+        else:
+            print("Found me two! x is ", x)
+    elif x > 2:
+        if x >= 3 and x - y <= 2:
+            print("Found me three! x is", x)
+    else:
+        if x % y > 1:
+            print("Found me four! x is", x)
 
-if -1 < x / y < 1:
-  if not x:
-    print("Found me! x is ", x)
-  else:
-    print("Found me two! x is ", x)
-elif x > 2:
-  if x >= 3 and x - y <= 2:
-    print("Found me three! x is", x)
-else:
-  if x % y > 1:
-    print("Found me four! x is", x)
 
 # 3.
 # Complete the code in the for loop
@@ -32,18 +33,18 @@ else:
 my_variable = 0
 for i in range(1, 10):
   # TODO add i to my_variable
-  ...
+  my_variable += i
 
   # TODO check if my_variable is even
-  if ...:
+  if my_variable % 2 == 0:
     # TODO square my_variable
-    ...
+    my_variable ** 2
   else:
     # TODO divide my_variable by 10
-    ...
+    my_variable / 10
 
   # TODO check if my_variable is greater than 100
-  if ...:
+  if my_variable > 100:
     break
 
 print(my_variable)
